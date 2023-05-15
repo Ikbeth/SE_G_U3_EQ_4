402e3891dcc8338fca12724fb935541fec79d910
PatoVII <119552013+PatoVII@users.noreply.github.com>
2023-05-14 21:52:31 -0600

    Add files via upload

--- Proyecto U3/Arduino/Proyecto_U3/Proyecto_U3.ino
+++ Proyecto U3/Arduino/Proyecto_U3/Proyecto_U3.ino
@@ -1,31 +1,28 @@
-int led1 = 2;
 int ps[] = { A0, A1, A2, A3, A4 };
 void setup() {
   Serial.begin(9600);
-  pinMode(led1, OUTPUT);
-  Serial.setTimeout(10);
+  Serial.setTimeout(1000);
 }
-int v;
+int lectura;
 void loop() {
   //CADENA = VMIN60
   if (Serial.available()) {
     //leemos con una variable tipo char que nos servira para elegir el tipo de eliminacion de ruido que se hara
     String cadena = Serial.readStringUntil('\n');
-    String opcion = cadena.substring(0,1);
-    String num = cadena.substring(2,cadena.length());
-    Serial.println(opcion);
-    Serial.println(num);
+    String opcion = cadena.substring(0, 1);
+    String num = cadena.substring(2, cadena.length());
     int tamMuestra = num.toInt();
-    
+
     //Esta opcion hara una muestra de 30 y se eliminara a travez de valor minimo
     //Declarar nuestro arreglo
-    int arr[] = {0, 0, 0, 0, 0};
+    int arr[] = { 0, 0, 0, 0, 0 };
     //Valor minimo
     if (opcion == "A") {
       int i, j, valMenor = 9999;
       for (j = 0; j < 5; j++) {
         for (i = 0; i < tamMuestra; i++) {
-          v = analogRead(ps[j]);
+          lectura = analogRead(ps[j]);
+          int v = map(lectura, 0, 1024, 1, 100);
           if (v < valMenor) {
             valMenor = v;
           }
@@ -33,7 +30,6 @@ void loop() {
         }
         valMenor = 9999;
       }
-      Serial.print("Valores del arreglo: ");
       Serial.println(String(arr[0]) + "," + String(arr[1]) + "," + String(arr[2]) + "," + String(arr[3]) + "," + String(arr[4]));
     }  //Fin valor minimo
        ///////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -42,7 +38,8 @@ void loop() {
       int i, j, valMenor = -1;
       for (j = 0; j < 5; j++) {
         for (i = 0; i < tamMuestra; i++) {
-          v = analogRead(ps[j]);
+          lectura = analogRead(ps[j]);
+          int v = map(lectura, 0, 1024, 1, 100);
           if (v > valMenor) {
             valMenor = v;
           }
@@ -50,7 +47,6 @@ void loop() {
         }
         valMenor = -1;
       }
-      Serial.print("Valores del arreglo: ");
       Serial.println(String(arr[0]) + "," + String(arr[1]) + "," + String(arr[2]) + "," + String(arr[3]) + "," + String(arr[4]));
     }  //Fin valor maximo
        /////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -59,13 +55,13 @@ void loop() {
       int i, j, sum = 0;
       for (j = 0; j < 5; j++) {
         for (i = 0; i < tamMuestra; i++) {
-          v = analogRead(ps[j]);
+          lectura = analogRead(ps[j]);
+          int v = map(lectura, 0, 1024, 1, 100);
           sum = sum + v;
         }
         arr[j] = sum / tamMuestra;
         sum = 0;
       }
-      Serial.print("Valores del arreglo: ");
       Serial.println(String(arr[0]) + "," + String(arr[1]) + "," + String(arr[2]) + "," + String(arr[3]) + "," + String(arr[4]));
     }  //Fin del promedio
        /////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -75,11 +71,20 @@ void loop() {
       int m[tamMuestra];
       for (j = 0; j < 5; j++) {
         for (i = 0; i < tamMuestra; i++) {
-          v = analogRead(ps[j]);
+          lectura = analogRead(ps[j]);
+          int v = map(lectura, 0, 1024, 1, 100);
           m[i] = v;
         }
+        for (int x = 0; x < tamMuestra; x++)
+          for (int y = 0; y < tamMuestra - 1; y++)
+            if (m[j] > m[j + 1]) {
+              // intercambiar arr[j] y arr[j+1]
+              int temp = m[j];
+              m[j] = m[j + 1];
+              m[j + 1] = temp;
+            }
         if (tamMuestra == 1) {
-          arr[j] = m[v];
+          arr[j] = m[i];
         } else {
           if (tamMuestra % 2 == 0) {
             int suma = (m[tamMuestra / 2] + m[(tamMuestra / 2) - 1]) / 2;
@@ -89,19 +94,28 @@ void loop() {
           }
         }
       }
-      Serial.print("Valores del arreglo: ");
       Serial.println(String(arr[0]) + "," + String(arr[1]) + "," + String(arr[2]) + "," + String(arr[3]) + "," + String(arr[4]));
     }  //Fin de la mediana
     ///////////////////////////////////////////////////////////////////////
     //Moda
     if (opcion == "E") {
-      int i, j, tamMuestra = 30;
+      int i, j;
       int m[tamMuestra];
       for (j = 0; j < 5; j++) {
         for (i = 0; i < tamMuestra; i++) {
-          v = analogRead(ps[j]);
+          lectura = analogRead(ps[j]);
+          int v = map(lectura, 0, 1024, 1, 100);
           m[i] = v;
         }
+        for (int x = 0; x < tamMuestra; x++)
+          for (int y = 0; y < tamMuestra - 1; y++)
+            if (m[j] > m[j + 1]) {
+              // intercambiar arr[j] y arr[j+1]
+              int temp = m[j];
+              m[j] = m[j + 1];
+              m[j + 1] = temp;
+            }
+
         int rMax = 0;
         for (int x = 0; x < tamMuestra; x++) {
           int r = 0;
@@ -116,9 +130,17 @@ void loop() {
           }
         }
       }
-      Serial.print("Valores del arreglo: ");
       Serial.println(String(arr[0]) + "," + String(arr[1]) + "," + String(arr[2]) + "," + String(arr[3]) + "," + String(arr[4]));
-    }  //Fin de la moda
+    }
+    ////////////////////////// tamMuestra = 1
+    if (opcion == "Q") {
+      int i;
+      for (i = 0; i < 5; i++) {
+        arr[i] = ps[i];
+      }
+      Serial.println(String(arr[0]) + "," + String(arr[1]) + "," + String(arr[2]) + "," + String(arr[3]) + "," + String(arr[4]));
+    }
   }
-  delay(500);
+
+  delay(1000);
 }
